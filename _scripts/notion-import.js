9c593e2236399fe3ef33572f2779bed6915459c7
victorcuervo <vcuervo@gmail.com>
2025-12-03 19:08:56 +0100

    Ajustes Import de Notion

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -4,7 +4,6 @@ import moment from "moment";
 import path from "path";
 import fs from "fs";
 
-
 const notion = new Client({
 	auth: process.env.NOTION_TOKEN,
 });
@@ -63,85 +62,49 @@ async function resolveSyncedBlocks(mdblocks) {
 
 		// date
 		let date = moment(r.created_time).format("YYYY-MM-DD")
-		let pdate = r.properties?.['Date']?.['date']?.['start']
+		let pdate = r.properties?.['LastUpdated Date']?.['date']?.['start']
 		if (pdate) {
 			date = moment(pdate).format('YYYY-MM-DD')
         }
         
         
-		// title
+		// Title
 		let title = ''
 		let ptitle = r.properties?.['Name']?.['title']
 		if (ptitle?.length > 0) {
 			title = ptitle[0]?.['plain_text']
 		}
 		
-		// excerpt
+		// Excerpt
 		let excerpt = ''
 		let pexcerpt = r.properties?.['Excerpt']?.['rich_text']
 		if (pexcerpt?.length > 0) {
 			excerpt = pexcerpt.map(text => text?.['plain_text']).join('')
 		}
-        
-		// tags
-		let tags = []
-		let pelementos = r.properties?.['Tags']?.['multi_select']
-		for (const t of pelementos) {
-			const n = t?.['name']
-			if (n) {
-				tags.push(n.toLowerCase())
-			}
-        }
 
-        let t = '[' + tags.toString() + ']'
-
-        // Categoría
+        // Category
        let cat = ''
        let pcats = r.properties?.['Category']?.['multi_select']
        cat = pcats[0]?.['name']
 
-       let nav = cat.toLowerCase();
+	   // SubCategory
+       let subcat = ''
+       let psubcats = r.properties?.['Subcategory']?.['multi_select']
+       subcat = psubcats[0]?.['name']
+
+       let nav = cat.toLowerCase() + '/' + subcat.toLowerCase();
 
 		// Slug
 		let slug = ''
 		let pslug = r.properties?.['Slug']?.['formula']
 		slug = pslug?.['string']
 
-		// image
-		let image= ''
-		let pimage = r.properties?.['Image']?.['rich_text']
-		if (pimage?.length > 0) {
-			image = pimage[0]?.['plain_text']
-		}
-
-		// image
-		let imageAutor= ''
-		let pimageAutor = r.properties?.['ImageAutor']?.['rich_text']
-		if (pimageAutor?.length > 0) {
-			imageAutor = pimageAutor[0]?.['plain_text']
-		}
-
-		// image
-		let imageLink= ''
-		let pimageLink = r.properties?.['ImageLink']?.['rich_text']
-		if (pimageLink?.length > 0) {
-			imageLink = pimageLink[0]?.['plain_text']
-		}		
-
+	
         
 const fm = `---
-layout: post
 title: ${title}
-excerpt: "${excerpt}"
-categories: ${cat}
-tags: ${t}
-image:
-  path: /images/${image}
-  thumbnail: /images/${image}`
-  + (imageAutor ? `
-  caption: Fotografía de [${imageAutor}](${imageLink})` : '') + `
-comments: true
-share: true
+description: "${excerpt}"
+lastupdates: ${date}
 author: victor_cuervo
 ---
 `
@@ -158,7 +121,7 @@ author: victor_cuervo
 	    fs.mkdirSync(root, { recursive: true })
 
 		//writing to file
-		const ftitle = `${date}-${slug}.md`
+		const ftitle = `${slug}.md`
 		fs.writeFile(path.join(root, ftitle), fm + md.parent, (err) => {
 			if (err) {
 				console.log(err);
