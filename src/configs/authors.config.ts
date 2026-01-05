c4980a0222864754061e6728caae38b6fe15c091
victorcuervo <vcuervo@gmail.com>
2026-01-05 01:10:42 +0100

    Configuraciones

--- src/configs/authors.config.ts
+++ src/configs/authors.config.ts
@@ -6,11 +6,11 @@ export const authors = [
         description: 'Programador, Arquitecto IT, álter ego de Línea de Código, amante de las tecnologías, generador de conocimiento y facilitador del aprendizaje.',
         rrss: [
             {
-                label: 'web',
+                label: 'laptop',
                 url: 'https://www.victorcuervo.com',
             },
             {
-                label: 'twitter',
+                label: 'x.com',
                 url: 'https://x.com/victor_cuervo',
             },
             {
@@ -35,4 +35,20 @@ export const authors = [
             },    
         ],
     },    
+    {
+        id: 'xhrist14n',
+        name: 'Christian Enrique Portilla Pauca',
+        photo: 'https://media.licdn.com/dms/image/v2/C4D03AQFpvJZ9VaQfdQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1561066892962?e=1767830400&v=beta&t=trllw00G69u1AXuzz5Quy6edXHAdpN9NT6KmTVPOYwU',
+        description: 'Devops Engineer / Site Reliability Engineer.',
+        rrss: [
+            {
+                label: 'web',
+                url: 'https://xhrist14n.wordpress.com/',
+            },
+            {
+                label: 'linkedin',
+                url: 'https://www.linkedin.com/in/xhrist14n/',
+            },            
+        ],
+    }
 ]
--- src/configs/sidebar.config.ts
+++ src/configs/sidebar.config.ts
@@ -3,6 +3,7 @@ export const sidebar = [
         label: 'Bootstrap',
         link: '/bootstrap/',
         icon: 'puzzle',
+        id: 'bootstrap',
         items: [
             {
             label: 'Básicos',
@@ -13,6 +14,7 @@ export const sidebar = [
         label: 'Ceylon',
         link: '/ceylon/',
         icon: 'seti:ceylon',
+        id: 'ceylon',
         items: [
             {
             label: 'Básicos',
@@ -22,16 +24,63 @@ export const sidebar = [
     {
         label: 'CSS',
         link: '/css/',
+        id: 'css',
         icon: 'seti:css',
         items: [
             {
             label: 'Básicos',
-            autogenerate: { directory: 'css' },
-            }],
+            autogenerate: { directory: 'css/básicos' },
+            },
+            {
+            label: 'Texto',
+            autogenerate: { directory: 'css/texto' },
+            },
+            {
+                label: 'Listas',
+                autogenerate: { directory: 'css/listas' },
+            },
+            {
+            label: 'Imágenes y Colores',
+            autogenerate: { directory: 'css/imágenes' },        
+            },
+            {
+            label: 'Box Model',
+            autogenerate: { directory: 'css/box-model' },
+            },
+            {
+            label: 'Layout',
+            autogenerate: { directory: 'css/layout' },
+            },
+            {
+            label: 'Grid',
+            autogenerate: { directory: 'css/grid' },
+            },
+            {
+            label: 'Flexbox',
+            autogenerate: { directory: 'css/flexbox' },
+            },            
+            {
+            label: 'Media Queries',
+            autogenerate: { directory: 'css/mediaquery' },
+            },
+            {
+            label: 'Transformaciones y Animaciones',
+            autogenerate: { directory: 'css/transformaciones' },
+            },
+            {
+            label: 'Variables y Funciones',
+            autogenerate: { directory: 'css/variables' },
+            },
+            {
+            label: 'Accesibilidad',
+            autogenerate: { directory: 'css/accesibilidad' },
+            },
+        ]
     },
     {
         label: 'Dart',
         link: '/dart/',
+        id: 'dart',
         icon: 'seti:dart',
         items: [
             {
@@ -42,6 +91,7 @@ export const sidebar = [
     {
         label: 'Dojo',
         link: '/dojo/',
+        id: 'dojo',
         icon: 'seti:dojo',
         items: [
             {
@@ -52,6 +102,7 @@ export const sidebar = [
     {
         label: 'DOM',
         link: '/dom/',
+        id: 'dom',
         icon: 'seti:html',
         items: [
             {
@@ -62,6 +113,7 @@ export const sidebar = [
     {
         label: 'EmberJS',
         link: '/emberjs/',
+        id: 'emberjs',
         icon: 'seti:javascript',
         items: [
             {
@@ -72,20 +124,47 @@ export const sidebar = [
     {
         label: 'HTML',
         link: '/html/',
+        id: 'html',
         icon: 'seti:html',
         items: [
             {
             label: 'Básicos',
-            autogenerate: { directory: 'html/basicos' },
+            autogenerate: { directory: 'html/básicos' },
+            },
+            {
+            label: 'Texto',
+            autogenerate: { directory: 'html/texto' },
+            },
+            {
+            label: 'Enlaces',
+            autogenerate: { directory: 'html/enlaces' },
+            },
+            {
+            label: 'Imágenes',
+            autogenerate: { directory: 'html/imágenes' },
             },
             {
             label: 'Formularios',
             autogenerate: { directory: 'html/formularios' },
-            }],
+            },
+            {
+            label: 'Frames',
+            autogenerate: { directory: 'html/frames' },
+            },
+            {
+            label: 'Entidades',
+            autogenerate: { directory: 'html/entidades' },
+            },
+            {
+            label: 'Vídeos',
+            autogenerate: { directory: 'html/vídeo' },
+            }
+        ],
     },
     {
         label: 'Google API',
         link: '/google/',
+        id: 'google',
         icon: 'seti:code-search',
         items: [
             {
@@ -96,6 +175,7 @@ export const sidebar = [
     {
         label: 'Groovy',
         link: '/groovy/',
+        id: 'groovy',
         icon: 'seti:java',
         items: [
             {
@@ -107,47 +187,184 @@ export const sidebar = [
         label: 'Java',
         link: '/java/',
         icon: 'seti:java',
+        id: 'java',
         items: [
             {
                 label: 'Básicos',							
                 autogenerate: { directory: 'java/basicos' },
             },
             {
-                label: 'Cadenas de texto',							
-                autogenerate: { directory: 'java/cadenas' },
+                label: 'Variables', 
+                autogenerate: { directory: 'java/variables' },
+            },
+            {
+                label: 'Operadores', 
+                autogenerate: { directory: 'java/operadores' },
+            },
+            {
+                label: 'Strings',
+                autogenerate: { directory: 'java/strings' },
             },
             {
                 label: 'Números',							
-                autogenerate: { directory: 'java/numeros' },
+                autogenerate: { directory: 'java/números' },
+            },
+            {
+                label: 'Validaciones',							
+                autogenerate: { directory: 'java/validaciones' },
+            },
+            {
+                label: 'Control de Flujo',							
+                autogenerate: { directory: 'java/control-de-flujo' },
+            },
+            {
+                label: 'Bucles',							
+                autogenerate: { directory: 'java/bucles' },
             },
             {
                 label: 'Arrays',							
                 autogenerate: { directory: 'java/arrays' },
             },
             {
+                label: 'Métodos',							
+                autogenerate: { directory: 'java/métodos' },
+            },
+            {
+                label: 'Fechas y Horas',							
+                autogenerate: { directory: 'java/fechas' },
+            },
+            {
                 label: 'Math',							
                 autogenerate: { directory: 'java/math' },
             },
             {
-                label: 'Ficheros',							
+                label: 'Matemáticos',
+                autogenerate: { directory: 'java/matemáticos' },
+            },
+            {
+                label: 'Excepciones',							
+                autogenerate: { directory: 'java/excepciones' },
+            },
+            {
+                label: 'Colecciones',							
+                autogenerate: { directory: 'java/colecciones' },
+            },
+            {
+                label: 'JDBC',							
+                autogenerate: { directory: 'java/jdbc' },
+            },
+            {
+                label: 'Clases y Objetos',							
+                autogenerate: { directory: 'java/clases' },
+            },
+            {
+                label: 'Mail',							
+                autogenerate: { directory: 'java/mail' },
+            },
+            {
+                label: 'Ficheros',
+                collapsed:true,							
                 autogenerate: { directory: 'java/ficheros' },
-            }            
+            },
+            {
+                label: 'Properties',							
+                autogenerate: { directory: 'java/properties' },
+            },
+            {
+                label: 'Redes',							
+                autogenerate: { directory: 'java/redes' },
+            },
+            {
+                label: 'Audio',							
+                autogenerate: { directory: 'java/audio' },
+            },
+            {
+                label: 'AWT',
+                autogenerate: { directory: 'java/awt' },
+            },
+            {
+                label: 'Swing',
+                autogenerate: { directory: 'java/swing' },
+            },
+            {
+                label: 'JSON',
+                autogenerate: { directory: 'java/json' },
+            },
+            {
+                label: 'Lambda',
+                autogenerate: { directory: 'java/lambda' },
+            },
+            {
+                label: 'Stream',
+                autogenerate: { directory: 'java/stream' },
+            },
+            {
+                label: 'Twitter API',							
+                autogenerate: { directory: 'java/twitter' },
+            },
+            {
+                label: 'Rendimiento',
+                autogenerate: { directory: 'java/rendimiento' },
+            },
+        ],
+    },
+    {
+        label: 'JavaEE',
+        link: '/javaee/',
+        icon: 'seti:java',
+        id: 'javaee',
+        items: [
+            {
+                label: 'Básicos',							
+                autogenerate: { directory: 'javaee/basicos' },
+            },
+            {
+                label: 'JSP',							
+                autogenerate: { directory: 'javaee/jsp' },
+            },
+            {
+                label: 'Servlets',
+                autogenerate: { directory: 'javaee/servlet' },
+            }         
         ],
     },
     {
         label: 'Javascript',
         link: '/javascript/',
         icon: 'seti:javascript',
+        id: 'javascript',
         items: [
             {
-            label: 'Reference',
-            autogenerate: { directory: 'javascript' },
-            }],
+            label: 'Básicos',
+            autogenerate: { directory: 'javascript/básicos' },
+            },
+            {
+            label: 'Bucles',
+            autogenerate: { directory: 'javascript/bucles' },
+            },
+            {
+            label: 'Arrays',
+            autogenerate: { directory: 'javascript/arrays' },
+            },
+            {
+            label: 'Funciones',
+            autogenerate: { directory: 'javascript/funciones' },
+            },
+            {
+            label: 'Objetos',
+            autogenerate: { directory: 'javascript/objetos' },
+            },
+            {
+                label: 'Expresiones Regulares',							
+                autogenerate: { directory: 'javascript/expresiones regulares' },
+            },
+        ],
     },
     {
         label: 'jQuery',
         link: '/jquery/',
         icon: 'seti:javascript',
+        id: 'jquery',
         items: [
             {
             label: 'Básicos',
@@ -158,6 +375,7 @@ export const sidebar = [
         label: 'MongoDB',
         link: '/mongodb/',
         icon: 'seti:db',
+        id: 'mongodb',
         items: [
             {
             label: 'Básicos',
@@ -168,6 +386,7 @@ export const sidebar = [
         label: '.Net',
         link: '/dotnet/',
         icon: 'seti:windows',
+        id: 'dotnet',
         items: [
             {
             label: 'Básicos',
@@ -178,6 +397,7 @@ export const sidebar = [
         label: 'NodeJS',
         link: '/nodejs/',
         icon: 'seti:nodejs',
+        id: 'nodejs',
         items: [
             {
             label: 'Básicos',
@@ -188,36 +408,77 @@ export const sidebar = [
         label: 'PHP',
         link: '/php/',
         icon: 'seti:php',
+        id: 'php',
         items: [
             {
-            label: 'Básicos',
-            autogenerate: { directory: 'php' },
-            }],
+                label: 'Básicos',
+                autogenerate: { directory: 'php/básicos' },
+            },
+            {
+                label: 'Operadores',
+                autogenerate: { directory: 'php/operadores' },
+            },
+            {
+                label: 'Cadenas',
+                autogenerate: { directory: 'php/cadenas' },
+            },
+            {
+                label: 'Arrays',
+                autogenerate: { directory: 'php/arrays' },
+            },
+            {
+                label: 'Clases',
+                autogenerate: { directory: 'php/clases' },
+            },
+            {
+                label: 'Bases de Datos',
+                autogenerate: { directory: 'php/bases-de-datos' },
+            },
+            {
+                label: 'Mail',
+                autogenerate: { directory: 'php/mail' },
+            }
+        ],
     },
     {
         label: 'Prototype',
         link: '/prototype/',
         icon: 'seti:javascript',
+        id: 'prototype',
         items: [
             {
-            label: 'Básicos',
-            autogenerate: { directory: 'prototype' },
-            }],
+                label: 'Básicos',
+                autogenerate: { directory: 'prototype/básicos' },
+            },
+            {
+                label: 'Capas',
+                autogenerate: { directory: 'prototype/capas' },
+            },
+            {
+                label: 'Formularios',
+                autogenerate: { directory: 'prototype/formularios' },
+            },
+        ],
     },
     {
         label: 'Python',
         link: '/python/',
         icon: 'seti:python',
+        id: 'python',
         items: [
             {
                 label: 'Básicos',
                 autogenerate: { directory: 'python/basicos' },
             },
             {
+                label: 'Cadenas',
+                autogenerate: { directory: 'python/cadenas' },
+            },
+            {
                 label: 'Bucles',
                 autogenerate: { directory: 'python/bucles' },
             },
-                        {
+            {
                 label: 'Ficheros',
                 autogenerate: { directory: 'python/ficheros' },
             }
@@ -227,6 +488,7 @@ export const sidebar = [
         label: 'ReactJS',
         link: '/reactjs/',
         icon: 'seti:react',
+        id: 'reactjs',
         items: [
             {
             label: 'Básicos',
@@ -235,18 +497,21 @@ export const sidebar = [
     },
     {
         label: 'Rust',
-        link: '/rust/',
+        link: '/rust/',    
         icon: 'seti:rust',
+        id: 'rust',
         items: [
             {
-            label: 'Básicos',
-            autogenerate: { directory: 'rust/basicos' },
-            }],
+                label: 'Básicos',
+                autogenerate: { directory: 'rust/básicos' },
+            }
+        ],
     },
     {
         label: 'SQL',
         link: '/sql/',
         icon: 'seti:db',
+        id: 'sql',
         items: [
             {
             label: 'Básicos',
@@ -257,16 +522,35 @@ export const sidebar = [
         label: 'SVG',
         link: '/svg/',
         icon: 'seti:svg',
+        id: 'svg',
         items: [
             {
-            label: 'Básicos',
-            autogenerate: { directory: 'svg' },
-            }],
+                label: 'Básicos',
+                autogenerate: { directory: 'svg/básicos' },
+            },
+            {
+                label: 'Líneas',
+                autogenerate: { directory: 'svg/líneas' },                
+            },
+            {
+                label: 'Figuras',
+                autogenerate: { directory: 'svg/figuras' },
+            },
+            {
+                label: 'Transparencias',
+                autogenerate: { directory: 'svg/transparencia' },                
+            },
+            {
+                label: 'Gradientes',
+                autogenerate: { directory: 'svg/gradientes' },
+            }
+        ],
     },
     {
         label: 'Symfony',
         link: '/symfony/',
         icon: 'seti:php',
+        id: 'symfony',
         items: [
             {
             label: 'Básicos',
@@ -277,6 +561,7 @@ export const sidebar = [
         label: 'Typescript',
         link: '/typescript/',
         icon: 'seti:typescript',
+        id: 'typescript',
         items: [
             {
             label: 'Básicos',
@@ -287,16 +572,27 @@ export const sidebar = [
         label: 'VBScript',
         link: '/vbscript/',
         icon: 'seti:purescript',
+        id: 'vbscript',
         items: [
             {
-            label: 'Básicos',
-            autogenerate: { directory: 'xml' },
-            }],
+                label: 'Básicos',
+                autogenerate: { directory: 'vbscript/básicos' },
+            },
+            {
+                label: 'Fechas y Horas',
+                autogenerate: { directory: 'vbscript/fechas' },
+            },
+            {
+                label: 'Arrays',
+                autogenerate: { directory: 'vbscript/arrays' },
+            }
+        ],
     },
     {
         label: 'wForms',
         link: '/wforms/',
         icon: 'forward-slash',
+        id: 'wforms',
         items: [
             {
             label: 'Básicos',
@@ -307,6 +603,7 @@ export const sidebar = [
         label: 'WordPress',
         link: '/wordpress/',
         icon: 'seti:notebook',
+        id: 'wordpress',
         items: [
             {
             label: 'Básicos',
@@ -317,6 +614,7 @@ export const sidebar = [
         label: 'XForms',
         link: '/xmlforms/',
         icon: 'seti:xml',
+        id: 'xforms',
         items: [
             {
             label: 'Básicos',
@@ -327,6 +625,7 @@ export const sidebar = [
         label: 'XML',
         link: '/xml/',
         icon: 'seti:xml',
+        id: 'xml',
         items: [
             {
                 label: 'Básicos',
