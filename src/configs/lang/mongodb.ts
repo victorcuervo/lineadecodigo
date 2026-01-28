62004603f6dec3b09b37fdda3c383f8b1c9dfc12
victorcuervo <vcuervo@gmail.com>
2026-01-28 18:51:02 +0100

    Cambios categorias MongoDB

--- src/configs/lang/mongodb.ts
+++ src/configs/lang/mongodb.ts
@@ -5,7 +5,64 @@ export const mongodb = {
     id: 'mongodb',
     items: [
         {
-        label: 'Básicos',
-        autogenerate: { directory: 'mongodb/basicos' },
-        }],
+            label: 'Básicos',
+            autogenerate: { directory: 'mongodb/básicos' },
+        },
+        {
+            label: 'Consultas',
+            autogenerate: { directory: 'mongodb/consultas' },
+        },
+        {
+            label: 'Operadores',
+            autogenerate: { directory: 'mongodb/operadores' },
+        },
+        {
+            label: 'Inserciones',
+            autogenerate: { directory: 'mongodb/inserciones' },
+        },
+        {
+            label: 'Actualizaciones',
+            autogenerate: { directory: 'mongodb/actualizaciones' },
+        },
+        {
+            label: 'Borrados',
+            autogenerate: { directory: 'mongodb/borrados' },
+        },
+        {
+            label: 'Cursores',
+            autogenerate: { directory: 'mongodb/cursores' },
+        },
+        {
+            label: 'Índices',
+            autogenerate: { directory: 'mongodb/índices' },
+        },
+        {
+            label: 'Modelado de Datos',
+            autogenerate: { directory: 'mongodb/modelado-de-datos' },
+        },
+        {
+            label: 'Aggregation Framework',
+            autogenerate: { directory: 'mongodb/aggregation-framework   ' },
+        },
+        {
+            label: 'Seguridad',
+            autogenerate: { directory: 'mongodb/seguridad' },
+        },
+        {
+            label: 'Replicación y Sharding',
+            autogenerate: { directory: 'mongodb/replicacion-y-sharding' },
+        },
+        {
+            label: 'Mantenimiento',
+            autogenerate: { directory: 'mongodb/mantenimiento' },
+        },
+        {
+            label: 'Monitorización',
+            autogenerate: { directory: 'mongodb/monitorizacion' },
+        },
+        {
+            label: 'Patrones',
+            autogenerate: { directory: 'mongodb/patrones' },
+        },
+    ],
 };
