1c7573807c242d7518585807b38d5bab3f0005f9
Victor <victor@w3api.com>
2026-09-13 23:43:58 +0200

    Eliminada categoría ADO

--- src/configs/lang/asp.ts
+++ src/configs/lang/asp.ts
@@ -7,5 +7,25 @@ export const asp = {
         {
             label: 'Básicos',
             autogenerate: { directory: 'asp/básicos' },
+        },
+        {
+            label: 'Sesión',
+            autogenerate: { directory: 'asp/sesión' },
+        },
+        {
+            label: 'Ficheros',
+            autogenerate: { directory: 'asp/ficheros' },
+        },
+        {
+            label: 'Cookies',
+            autogenerate: { directory: 'asp/cookies' },
+        },
+        {
+            label: 'Bases de Datos',
+            autogenerate: { directory: 'asp/bases de datos' },
+        },
+        {
+            label: 'Mail',
+            autogenerate: { directory: 'asp/mail' },
         }],
 };
